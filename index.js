d709351e344ea336ded7b6968ecb2015723e9a7f
stephen <stevedogbey@gmail.com>
2022-06-28 14:49:22 +0000

    commit tree 832317cc8c610b1b80e2544f60049b0d8c87705d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+//Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
+// document.getElementById("main").remove();
+
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory";
+// newHeader.innerHTML = "YOUR-NAME is the champion";
